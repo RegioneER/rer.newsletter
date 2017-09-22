d3102596ea2c69e2da8a05fde250294d3d18a3db
Filippo Campi <filippocampi@Filippos-MacBook.local>
2017-09-22 12:25:30 +0200

    [dev] added subscribe method definition

--- src/rer/newsletter/utility/newsletter.py
+++ src/rer/newsletter/utility/newsletter.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*-
 from zope.interface import Interface
 
+# return type
+UNHANDLED = 0
+SUBSCRIBED = 1
+ALREADY_SUBSCRIBED = 2
+INVALID_EMAIL = 3
+
 
 class INewsletterUtility(Interface):
     """ """
 
     def subscribe(mail):
-        """ """
+        """
+        Subscribe to newsletter
+
+        Args:
+            newsletter (str): newsletter id
+            mail (str): email address
+
+        Returns:
+            int: SUBSCRIBED (1) if succesful, ALREADY_SUBSCRIBED (2),
+                INVALD_EMAIL (3) otherwise.
+
+        Raises:
+        """
