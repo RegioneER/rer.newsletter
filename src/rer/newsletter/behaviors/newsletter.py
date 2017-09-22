cb8c99e046012ec467f630b20febdbeb8a488d9e
Filippo Campi <filippocampi@Filippos-MacBook.local>
2017-09-22 15:08:39 +0200

    [dev] added schema to newsletters behavior

--- src/rer/newsletter/behaviors/newsletter.py
+++ src/rer/newsletter/behaviors/newsletter.py
@@ -5,6 +5,23 @@ from zope import schema
 from zope.interface import alsoProvides
 from plone.autoform.interfaces import IFormFieldProvider
 
+# constraint
+from zope.interface import Invalid
+import re
+
+
+def mailValidation(mailList):
+
+    for mail in mailList:
+        # valido la mail
+        match = re.match(
+            '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$',
+            mail
+        )
+        if match is None:
+            raise Invalid(u"Una o piu delle mail inserite non sono valide")
+    return True
+
 
 class INewsletter(model.Schema):
     """
@@ -14,8 +31,26 @@ class INewsletter(model.Schema):
     active = schema.Bool(
         title=_("active_newsletter", default="Active newsletter"),
         description=_("description_active_newsletter",
-                      default="Make newsletter active")
+                      default="Make newsletter active"),
+        required=False
     )
+
+    sender_name = schema.TextLine(
+        title=_("sender_name", default="Sender's Fullname"),
+        description=_("description_sender_name",
+                      default="Fullname of sender"),
+        required=False
+    )
+
+    sender_email = schema.TextLine(
+        title=_("sender_email", default="Sender's email"),
+        description=_("description_sender_email",
+                      default="Email of sender"),
+        required=False,
+        constraint=mailValidation
+    )
+
+
 
 
 alsoProvides(INewsletter, IFormFieldProvider)
