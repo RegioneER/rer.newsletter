bd4349a41b39c9b4ddfb3a289fdde153430f4298
Filippo Campi <filippocampi@Filippos-MacBook.local>
2017-10-26 18:24:46 +0200

    [dev] added: template and class for view for message and newsletter

---
+++ src/rer/newsletter/browser/newsletter/newsletterview.py
@@ -0,0 +1,19 @@
+from Products.Five.browser import BrowserView
+from plone import api
+
+
+class NewsletterView(BrowserView):
+
+    def getMessageList(self):
+
+        if self.context.portal_type == 'Newsletter':
+            messageList = api.content.find(
+                context=self.context,
+                portal_type='Message'
+            )
+
+            ml = []
+            for message in messageList:
+                ml.append(message.getObject())
+
+            return ml
