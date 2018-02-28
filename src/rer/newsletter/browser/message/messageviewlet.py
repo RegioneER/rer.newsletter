6b1d4300d824f9c6304a9590dc535bea4e846fa5
Filippo Campi <filippo.campi@redturtle.it>
2018-02-28 17:59:34 +0100

    [fix] fixed message workflow

--- src/rer/newsletter/browser/message/messageviewlet.py
+++ src/rer/newsletter/browser/message/messageviewlet.py
@@ -13,9 +13,11 @@ class MessageManagerViewlet(ViewletBase):
         pass
 
     def canManageNewsletter(self):
-        if 'Editor' in api.user.get_roles(obj=self.context) \
-                or api.user.get_permissions(obj=self.context).get(
-                'rer.newsletter: Manage Newsletter'):
+        isEditor = 'Editor' in api.user.get_roles(obj=self.context)
+        hasManageNewsletter = api.user.get_permissions(obj=self.context).get(
+            'rer.newsletter: Manage Newsletter') and 'Gestore Newsletter' not \
+            in api.user.get_roles(obj=self.context)
+        if isEditor or hasManageNewsletter:
             return True
         else:
             return False
@@ -23,10 +25,10 @@ class MessageManagerViewlet(ViewletBase):
     def canSendMessage(self):
         if (api.content.get_state(obj=self.context) == 'published'
                 and api.user.get_permissions(obj=self.context).get(
-                'rer.newsletter: Send Newsletter')) \
-                or (api.content.get_state(obj=self.context) == 'published'
-                    and 'Gestore Newsletter' in api.user.get_roles(
-                    obj=self.context)):
+                'rer.newsletter: Send Newsletter')):
+                # or (api.content.get_state(obj=self.context) == 'published'
+                #     and 'Gestore Newsletter' in api.user.get_roles(
+                #     obj=self.context)):
             return True
         else:
             return False
