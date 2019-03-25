109cb93214e62ca636edcf7f69f1fb03d9e9c284
Filippo Campi <filippo.campi@redturtle.it>
2019-03-25 15:49:58 +0100

    [fix] fixed bad quotes problem

--- src/rer/newsletter/browser/message/versionview.py
+++ src/rer/newsletter/browser/message/versionview.py
@@ -5,12 +5,12 @@ from zope.component import getMultiAdapter
 
 class MessageVersionView(VersionView):
     def __call__(self):
-        version_id = self.request.get("version_id", None)
+        version_id = self.request.get('version_id', None)
         if not version_id:
-            raise ValueError(u"Missing parameter on the request: version_id")
+            raise ValueError(u'Missing parameter on the request: version_id')
 
         content_core_view = getMultiAdapter(
-            (self.context, self.request), name="content-core"
+            (self.context, self.request), name='content-core'
         )
         html = content_core_view()
         return self._convert_download_links(html, version_id)
