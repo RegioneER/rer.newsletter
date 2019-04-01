efa5bf18173b4068dc45a4d38f9478064d0cca38
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-04-01 15:56:03 +0200

    Back to development: 0.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='rer.newsletter',
-    version='0.2.0',
+    version='0.2.1.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
