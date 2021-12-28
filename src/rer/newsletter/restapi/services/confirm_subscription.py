2fe65d9de8b4a0829e9c30cfc611b7ad7a5ac293
Martina Bustacchini <martina.bustacchini@redturtle.it>
2021-12-28 13:17:10 +0100

    [dev] added restapi services for Volto usage

---
+++ src/rer/newsletter/restapi/services/confirm_subscription.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+from plone import api
+from rer.newsletter import _
+from rer.newsletter.adapter.subscriptions import IChannelSubscriptions
+from rer.newsletter.contentrules.events import SubscriptionEvent
+from rer.newsletter.contentrules.events import UnsubscriptionEvent
+from rer.newsletter.utils import OK
+from rer.newsletter.utils import compose_sender
+from rer.newsletter.utils import get_site_title
+from zope.component import getMultiAdapter
+from zope.event import notify
+from plone import api
+from plone.restapi.deserializer import json_body
+from plone.restapi.services import Service
+from rer.newsletter import _
+from rer.newsletter import logger
+from rer.newsletter.adapter.subscriptions import IChannelSubscriptions
+from six import PY2
+from zope.component import getMultiAdapter
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class NewsletterConfirmSubscription(Service):
+
+    def _sendGenericMessage(self, template, receiver, message, message_title):
+        mail_template = self.context.restrictedTraverse(
+            "@@{0}".format(template)
+        )
+
+        parameters = {
+            "header": self.context.header,
+            "footer": self.context.footer,
+            "style": self.context.css_style,
+            "portal_name": get_site_title(),
+            "channel_name": self.context.title,
+        }
+
+        mail_text = mail_template(**parameters)
+
+        portal = api.portal.get()
+        mail_text = portal.portal_transforms.convertTo("text/mail", mail_text)
+
+        response_email = compose_sender(self.context)
+
+        # invio la mail ad ogni utente
+        mail_host = api.portal.get_tool(name="MailHost")
+        mail_host.send(
+            mail_text.getData(),
+            mto=receiver,
+            mfrom=response_email,
+            subject=message_title,
+            charset="utf-8",
+            msg_type="text/html",
+        )
+
+        return OK
+
+    def reply(self):
+        data = json_body(self.request)
+        secret = data.get("secret")
+        action = data.get("action")
+        error = None
+        response = None
+        channel = getMultiAdapter(
+            (self.context, self.request), IChannelSubscriptions
+        )
+
+        if action == u"subscribe":
+            response, user = channel.activateUser(secret=secret)
+            # mandare mail di avvenuta conferma
+            if response == OK:
+                notify(SubscriptionEvent(self.context, user))
+                self._sendGenericMessage(
+                    template="activeuserconfirm_template",
+                    receiver=user,
+                    message="Messaggio di avvenuta iscrizione",
+                    message_title="Iscrizione confermata",
+                )
+                status = u"generic_activate_message_success"
+
+        elif action == u"unsubscribe":
+            response, mail = channel.deleteUserWithSecret(secret=secret)
+            if response == OK:
+                notify(UnsubscriptionEvent(self.context, mail))
+                status = u"generic_delete_message_success"
+
+        if response != OK:
+            logger.error(
+                'Unable to unsubscribe user with token "{token}" on channel {channel}.'.format(  # noqa
+                    token=secret, channel=channel.absolute_url()
+                )
+            )
+            status = u"unable_to_unsubscribe"
+            error = u"unable_to_unsubscribe"
+
+        return {
+            "@id": self.request.get("URL"),
+            "status": status,
+            "error": error,
+        }
---
+++ src/rer/newsletter/restapi/services/subscribe.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+from Acquisition import aq_inner
+from plone import api
+from plone.restapi.deserializer import json_body
+from plone.protect.authenticator import createToken
+from plone.restapi.services import Service
+from rer.newsletter import _
+from rer.newsletter import logger
+from rer.newsletter.adapter.subscriptions import IChannelSubscriptions
+from rer.newsletter.utils import compose_sender, get_site_title, SUBSCRIBED, UNHANDLED
+from six import PY2
+from zope.component import getMultiAdapter
+
+
+class NewsletterSubscribe(Service):
+
+    def getData(self, data):
+        errors = {}
+        if not data.get("email", None):
+            errors['email'] = u"invalid_email"
+        if not data.get("g-recaptcha-response", None):
+            errors['g-recaptcha-response'] = u"invalid_captcha"
+        return {
+            "email": data.get("email", None),
+            'g-recaptcha-response': data.get('g-recaptcha-response', None),
+        }, errors
+
+    def handleSubscribe(self, postData):
+        status = UNHANDLED
+        data, errors = self.getData(postData)
+
+        # recaptcha
+        captcha = getMultiAdapter(
+            (aq_inner(self.context), self.request), name="captcha"
+        )
+        if errors:
+           return data, errors
+
+        self.request['g-recaptcha-response'] = data['g-recaptcha-response']
+        # come funziona/ come testarlo?
+        if not captcha.verify():
+            errors = u"message_wrong_captcha"
+
+            return data, errors
+
+        email = data.get("email", "").lower()
+
+        if self.context.is_subscribable:
+            channel = getMultiAdapter(
+                (self.context, self.request), IChannelSubscriptions
+            )
+            status, secret = channel.subscribe(email)
+
+        if status == SUBSCRIBED:
+
+            # creo il token CSRF
+            token = createToken()
+
+            # mando mail di conferma
+            url = self.context.absolute_url()
+            url += "/confirm-subscription?secret=" + secret
+            url += "&_authenticator=" + token
+            url += "&action=subscribe"
+
+            mail_template = self.context.restrictedTraverse(
+                "@@activeuser_template"
+            )
+
+            parameters = {
+                "title": self.context.title,
+                "header": self.context.header,
+                "footer": self.context.footer,
+                "style": self.context.css_style,
+                "activationUrl": url,
+                "portal_name": get_site_title(),
+            }
+
+            mail_text = mail_template(**parameters)
+
+            portal = api.portal.get()
+            mail_text = portal.portal_transforms.convertTo(
+                "text/mail", mail_text
+            )
+            sender = compose_sender(channel=self.context)
+
+            channel_title = self.context.title
+            if PY2:
+                channel_title = self.context.title.encode("utf-8")
+
+            mailHost = api.portal.get_tool(name="MailHost")
+            mailHost.send(
+                mail_text.getData(),
+                mto=email,
+                mfrom=sender,
+                subject="Conferma la tua iscrizione alla Newsletter {channel}"
+                " del portale {site}".format(
+                    channel=channel_title, site=get_site_title()
+                ),
+                charset="utf-8",
+                msg_type="text/html",
+                immediate=True,
+            )
+            return data, errors
+
+        else:
+            if status == 2:
+                logger.exception("user already subscribed")
+                errors = u"user_already_subscribed"
+                return data, errors
+            else:
+                logger.exception("unhandled error subscribe user")
+                errors = u"Problems...{0}".format(status)
+                return data, errors
+
+    def reply(self):
+        data = json_body(self.request)
+        _data, errors = self.handleSubscribe(data)
+
+        return {
+            "@id": self.request.get("URL"),
+            "errors": errors if errors else None,
+            'status': u"user_subscribe_success" if not errors else 'error',
+        }
+
