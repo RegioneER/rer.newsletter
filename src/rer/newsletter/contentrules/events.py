bb57be631fb376d690ed8ed5fa8e3f1704e31489
Filippo Campi <filippo.campi@redturtle.it>
2023-03-10 09:16:04 +0100

    [dev] plone6 compatibility

--- src/rer/newsletter/contentrules/events.py
+++ src/rer/newsletter/contentrules/events.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-from zope.component.interfaces import IObjectEvent
-from zope.component.interfaces import ObjectEvent
+try:
+    from zope.component.interfaces import IObjectEvent
+    from zope.component.interfaces import ObjectEvent
+except ImportError:
+    from zope.interface.interfaces import IObjectEvent
+    from zope.interface.interfaces import ObjectEvent
+
 from zope.interface import implementer
 
 
