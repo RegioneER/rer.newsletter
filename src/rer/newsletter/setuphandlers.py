25689adadb8b4e86b8292f2efc7a024f0abd09fb
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-02-07 16:50:51 +0100

    remove not needed profile

--- src/rer/newsletter/setuphandlers.py
+++ src/rer/newsletter/setuphandlers.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from .transforms.link_transform import link_transform
 from .transforms.mimetype import mail
-from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from Products.MimetypesRegistry.interfaces import IMimetypesRegistryTool
 from Products.PortalTransforms.interfaces import IPortalTransformsTool
@@ -13,7 +12,7 @@ from zope.interface import implementer
 class HiddenProfiles(object):
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller"""
-        return ["rer.newsletter:uninstall", "rer.newsletter:plone5"]
+        return ["rer.newsletter:uninstall"]
 
 
 def post_install(context):
@@ -26,12 +25,6 @@ def post_install(context):
     # registro la mia custom transform
     pt = getUtility(IPortalTransformsTool)
     pt.registerTransform(link_transform())
-
-    if api.env.plone_version() < "6.0":
-        setup_tool = api.portal.get_tool("portal_setup")
-        setup_tool.runImportStepFromProfile(
-            "profile-rer.newsletter:plone5", "plone.app.registry"
-        )
 
 
 def uninstall(context):
